de52d930bfb3b0c6d6cf9c26255c90d74c6e1443
Mihail Schupakevich <m.schupakevich@nv.dunice.net>
2024-09-09 11:21:14 +0300

    GORM connection+EP

--- go.mod
+++ go.mod
@@ -29,7 +29,10 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgtype v1.14.3 // indirect
 	github.com/jackc/pgx/v4 v4.18.3 // indirect
+	github.com/jackc/pgx/v5 v5.7.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
@@ -44,8 +47,11 @@ require (
 	golang.org/x/arch v0.10.0 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/postgres v1.5.9 // indirect
+	gorm.io/gorm v1.25.11 // indirect
 )
--- main.go
+++ main.go
@@ -1,89 +1,129 @@
 package main
 
 import (
-	"database/sql"
-	"github.com/gin-gonic/gin"
+	"encoding/json"
 	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"log"
+	"net/http"
+	"time"
 )
 
-type User struct {
-	ID    int
-	Email string
+type Task struct {
+	ID        uint      `gorm:"primaryKey"`
+	Text      string    `gorm:"type:text"`
+	Checkbox  bool      `gorm:"default:false"`
+	CreatedAt time.Time `gorm:"index"`
+	UpdatedAt time.Time `gorm:"index"`
 }
 
-func ConnectDB() (*sql.DB, error) {
-	db, err := sql.Open("postgres", "dbname=GoDB user=admin password=admin port=5432")
-	if err != nil {
-		return nil, err
-	}
-	return db, nil
-}
-
-var users []User
+var tasks []Task
 
 func main() {
-	db, err := ConnectDB()
+	dsn := "host=localhost user=admin dbname=GoDB password=admin sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	defer db.Close()
+	db.AutoMigrate(&Task{})
 
-	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello, World!",
-		})
+	//получение всех записей
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		db.Find(&tasks)
+		json.NewEncoder(w).Encode(tasks)
 	})
 
-	r.GET("/users", func(c *gin.Context) {
-
-		rows, err := db.Query("SELECT * FROM users")
-		if err != nil {
-			c.JSON(500, gin.H{"error": "Failed to query database"})
+	//создание 1 записи
+	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
 		}
-		defer rows.Close()
-
-		for rows.Next() {
-			var user User
-			err := rows.Scan(&user.ID, &user.Email)
-			if err != nil {
-				c.JSON(500, gin.H{"error": "Failed to scan row"})
-				return
-			}
-			users = append(users, user)
+		var task Task
+		err := json.NewDecoder(r.Body).Decode(&task)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 		}
-
-		c.JSON(200, gin.H{"users": users})
+		result := db.Create(&task)
+		if result.Error != nil {
+			http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+			return
+		}
+		if task.Text == "" {
+			http.Error(w, "Empty task text", http.StatusBadRequest)
+		}
+		tasks = append(tasks, task)
+		json.NewEncoder(w).Encode("task succesfully created")
 	})
-
-	r.POST("/user", func(c *gin.Context) {
-		var user User
-		err := c.BindJSON(&user)
-		if err != nil {
-			c.JSON(400, gin.H{"error": "Неверный запрос"})
-			return
-		}
-
-		// Check if the user struct is empty
-		if user.Email == "" {
-			c.JSON(400, gin.H{"error": "Электронная почта обязательна"})
-			return
-		}
-
-		_, err = db.Exec("INSERT INTO users (email) VALUES ($1) RETURNING id", user.Email)
-		if err != nil {
-			c.JSON(500, gin.H{"error": "Не удалось выполнить запрос к базе данных"})
-			return
-		}
-		users = append(users, user)
-		c.JSON(201, gin.H{
-			"message": "Successfully create user",
-		})
-	})
-
-	r.Run(":8080")
-
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+//var users []User
+//
+//func main() {
+//	db, err := ConnectDB()
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	defer db.Close()
+//
+//	r := gin.Default()
+//
+//	r.GET("/", func(c *gin.Context) {
+//		c.JSON(200, gin.H{
+//			"message": "Hello, World!",
+//		})
+//	})
+//
+//	r.GET("/users", func(c *gin.Context) {
+//
+//		rows, err := db.Query("SELECT * FROM users")
+//		if err != nil {
+//			c.JSON(500, gin.H{"error": "Failed to query database"})
+//			return
+//		}
+//		defer rows.Close()
+//
+//		for rows.Next() {
+//			var user User
+//			err := rows.Scan(&user.ID, &user.Email)
+//			if err != nil {
+//				c.JSON(500, gin.H{"error": "Failed to scan row"})
+//				return
+//			}
+//			users = append(users, user)
+//		}
+//
+//		c.JSON(200, gin.H{"users": users})
+//	})
+//
+//	r.POST("/user", func(c *gin.Context) {
+//		var user User
+//		err := c.BindJSON(&user)
+//		if err != nil {
+//			c.JSON(400, gin.H{"error": "Неверный запрос"})
+//			return
+//		}
+//
+//		// Check if the user struct is empty
+//		if user.Email == "" {
+//			c.JSON(400, gin.H{"error": "Электронная почта обязательна"})
+//			return
+//		}
+//
+//		_, err = db.Exec("INSERT INTO users (email) VALUES ($1) RETURNING id", user.Email)
+//		if err != nil {
+//			c.JSON(500, gin.H{"error": "Не удалось выполнить запрос к базе данных"})
+//			return
+//		}
+//		users = append(users, user)
+//		c.JSON(201, gin.H{
+//			"message": "Successfully create user",
+//		})
+//	})
+//
+//	r.Run(":8080")
+//
+//}
