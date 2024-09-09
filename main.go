a5fe9c532d24da1b214cc84e974109627a0c44d4
Mihail Schupakevich <m.schupakevich@nv.dunice.net>
2024-09-09 17:04:15 +0300

    all_EP

--- main.go
+++ main.go
@@ -1,21 +1,19 @@
 package main
 
 import (
-	"encoding/json"
+	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
-	"net/http"
 	"time"
 )
 
 type Task struct {
 	ID        uint      `gorm:"primaryKey"`
-	Text      string    `gorm:"type:text"`
-	Checkbox  bool      `gorm:"default:false"`
-	CreatedAt time.Time `gorm:"index"`
-	UpdatedAt time.Time `gorm:"index"`
+	Text      string    `gorm:"type:text" json:"text"`
+	Checkbox  bool      `gorm:"type:boolean" json:"checkbox"`
+	CreatedAt time.Time `gorm:"index default current_timestamp()"`
+	UpdatedAt time.Time `gorm:"index default current_timestamp()"`
 }
 
 var tasks []Task
@@ -28,102 +26,64 @@ func main() {
 	}
 	db.AutoMigrate(&Task{})
 
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ChiVa<1",
+		})
+	})
 	//получение всех записей
-	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+	r.GET("/tasks", func(c *gin.Context) {
 		db.Find(&tasks)
-		json.NewEncoder(w).Encode(tasks)
+		c.JSON(200, gin.H{"tasks": tasks})
 	})
 
 	//создание 1 записи
-	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
+	r.POST("/task", func(c *gin.Context) {
 		var task Task
-		err := json.NewDecoder(r.Body).Decode(&task)
+		c.BindJSON(task)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		result := db.Create(&task)
-		if result.Error != nil {
-			http.Error(w, result.Error.Error(), http.StatusInternalServerError)
 			return
 		}
 		if task.Text == "" {
-			http.Error(w, "Empty task text", http.StatusBadRequest)
+			c.JSON(400, gin.H{"error": "Text is empty!"})
+			return
 		}
 		tasks = append(tasks, task)
-		json.NewEncoder(w).Encode("task succesfully created")
+		db.Create(&task)
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
 
-//var users []User
-//
-//func main() {
-//	db, err := ConnectDB()
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	defer db.Close()
-//
-//	r := gin.Default()
-//
-//	r.GET("/", func(c *gin.Context) {
-//		c.JSON(200, gin.H{
-//			"message": "Hello, World!",
-//		})
-//	})
-//
-//	r.GET("/users", func(c *gin.Context) {
-//
-//		rows, err := db.Query("SELECT * FROM users")
-//		if err != nil {
-//			c.JSON(500, gin.H{"error": "Failed to query database"})
-//			return
-//		}
-//		defer rows.Close()
-//
-//		for rows.Next() {
-//			var user User
-//			err := rows.Scan(&user.ID, &user.Email)
-//			if err != nil {
-//				c.JSON(500, gin.H{"error": "Failed to scan row"})
-//				return
-//			}
-//			users = append(users, user)
-//		}
-//
-//		c.JSON(200, gin.H{"users": users})
-//	})
-//
-//	r.POST("/user", func(c *gin.Context) {
-//		var user User
-//		err := c.BindJSON(&user)
-//		if err != nil {
-//			c.JSON(400, gin.H{"error": "Неверный запрос"})
-//			return
-//		}
-//
-//		// Check if the user struct is empty
-//		if user.Email == "" {
-//			c.JSON(400, gin.H{"error": "Электронная почта обязательна"})
-//			return
-//		}
-//
-//		_, err = db.Exec("INSERT INTO users (email) VALUES ($1) RETURNING id", user.Email)
-//		if err != nil {
-//			c.JSON(500, gin.H{"error": "Не удалось выполнить запрос к базе данных"})
-//			return
-//		}
-//		users = append(users, user)
-//		c.JSON(201, gin.H{
-//			"message": "Successfully create user",
-//		})
-//	})
-//
-//	r.Run(":8080")
-//
-//}
+	//обновление таски?
+	r.PATCH("/task/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var task Task
+		var updateTask Task
+		if err := c.BindJSON(&updateTask); err != nil {
+			c.JSON(400, gin.H{"error": "Invalid request"})
+			return
+		}
+		if updateTask.Text == "" {
+			c.JSON(400, gin.H{"error": "Text is empty!"})
+			return
+		}
+		if err := db.Model(&task).Where("id = ?", id).Update("checkbox", updateTask.Checkbox).Error; err != nil {
+			c.JSON(500, gin.H{"error": "Failed to update task"})
+			return
+		}
+		c.JSON(200, gin.H{"message": "Task updated successfully"})
+
+	})
+
+	//delete task
+	r.DELETE("/task/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		result := db.First(&Task{}, id)
+		db.Delete(&Task{}, result)
+		if result.Error != nil {
+			c.JSON(401, gin.H{"error": result.Error.Error()})
+		}
+	})
+
+	r.Run(":8080")
+}
