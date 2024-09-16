d3fd30a73ad8250064d6e1a731418150d4c24040
Mihail Schupakevich <m.schupakevich@nv.dunice.net>
2024-09-16 17:26:45 +0300

    swagger

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
+	"awesomeProject/cmd/docs"
 	"awesomeProject/internal/api/handlers"
 	"awesomeProject/pkg/db"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+	_ "github.com/swaggo/files" // swagger embed files
+	swaggerFiles "github.com/swaggo/files"
+	_ "github.com/swaggo/gin-swagger" // gin-swagger middleware
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"log"
 )
 
+// @title ToDo API
+// @version 1.0
+// @description Description of the ToDO REST API
+// @Summary      List tasks
+// @Description  get tasks
+// @Tags         tasks
+// @Accept       json
+// @Produce      json
+// @Router       /tasks [get]
+// @host localhost:8080
+// @BasePath /tasks
 func main() {
 	db1, err := db.Сonnection()
 	if err != nil {
-		log.Fatal(err) // handle error when db connection fails
+		log.Fatal(err)
 	}
 
-	// Инициализация роутинга
 	serve := gin.New()
+
 	routes := serve.Group("/tasks")
 	if db1 != nil {
 		taskHandler := handlers.TaskHandler{DB: db1}
@@ -23,8 +39,12 @@ func main() {
 		routes.POST("/", taskHandler.Post)
 		routes.PATCH("/:id", taskHandler.Update)
 		routes.DELETE("/:id", taskHandler.Delete)
+		routes.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	} else {
 		log.Fatal("Failed to establish database connection")
 	}
+
+	docs.Init()
+
 	serve.Run(":8080")
 }
